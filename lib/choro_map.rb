0547ccf8c7bf58c2dab23d059093fd5c02d695b0
Jacob Perkins <jacob.a.perkins@gmail.com>
2010-07-12 00:33:47 -0500

    made script a whole lot less brittle, now can choose which color scheme, added sample data. script to get from lat lon to county code (given the appropriate dataset from Infochimps) to come soon

--- lib/choro_map.rb
+++ lib/choro_map.rb
@@ -1,67 +1,94 @@
 #!/usr/bin/env ruby
 
-#
-# Make me a choropleth map baby.
-#
-# Modified from:
-# http://flowingdata.com/2009/11/12/how-to-make-a-us-county-thematic-map-using-free-tools/
-#
-# USAGE:
-#
-#        ./choro_map.rb <datafile> <map> > <output.svg>
-#
-# Where we assume your <datafile> has rows that look like: "identifier "\t" count".
-# Here "identifier" is a county code or state abbreviation on whether <map>
-# is "usa" or "counties".
-#
-
 require 'rubygems'
 require 'nokogiri'
 
-datafilename = ARGV[0]
-map = ARGV[1]
+class Choromap
+  attr_accessor :counts, :svg_map, :colors
 
-data = File.open(datafilename, 'r')
-
-# First, read the data into a hash
-counts = { }
-data.each do |row|
-  identifier = row.split[0]
-  counts[identifier] = row.split[1]
-end
-
-# Next, open the svg map file
-mapname = "maps/" + map + ".svg"
-svg_map = Nokogiri::XML(open(mapname))
-
-# Find all paths in the file
-paths = svg_map.css('path')
-
-# Next, set colors and path styles. This is an awful color scheme, by the way.
-colors = ["#FFFFCC", "#C2E699", "#78C679", "#31A354", "#006837"]
-path_style = 'font-size:12px;fill-rule:nonzero;stroke:#FFFFFF;stroke-opacity:1;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-linecap:butt;marker-start:none;stroke-linejoin:bevel;fill:'
-
-# Then, go through each path and set its color based on its count
-#
-# Note: the numbers used in the comparisons below will likely have to change depending
-# on the scale of your data.
-paths.each do |p|
-  count = counts[p['id']].to_f
-  next if p['id'].eql?("path57")
-  if count > 100
-    color_class = 4
-  elsif count > 75
-    color_class = 3
-  elsif count > 50
-    color_class = 2
-  elsif count > 25
-    color_class = 1
-  else
-    color_class = 0
+  def initialize(filename, options = {:color_scheme => 'YlOrRd'})
+    @counts = {}
+    @colors = hex_color_array(options[:color_scheme])
+    read(filename)
   end
-  color = colors[color_class]
-  p['style'] = path_style + color
+
+  #
+  # Data file MUST be of the form:
+  #
+  # county_code'\t'count
+  #
+  # OR
+  #
+  # state_abbrv'\t'count
+  #
+  def read filename
+    File.readlines(filename).each do |line|
+      row = line.strip.split
+      counts[row.first] = row.last.to_i
+    end
+  end
+
+  def choropleth! map_type
+    mapname = "maps/" + map_type + ".svg"
+    @svg_map = Nokogiri::XML(open(mapname))
+    paths    = svg_map.css('path')
+    edit_paths paths
+    @svg_map.display
+  end
+
+  
+  #
+  # Color themes direct from colorbrewer2.org
+  #
+  def hex_color_array color_scheme
+    case color_scheme
+    when 'YlOrRd' then
+      ["#FFFFB2", "#FED976", "#FEB24C", "#FD8D3C", "#FC4E2A", "#E31A1C", "#B10026"]
+    when 'RdPu' then
+      ["#FEEBE2", "#FCC5C0", "#FA9FB5", "#F768A1", "#DD3497", "#AE017E", "#7A0177"]
+    when 'YlGn' then
+      ["#FFFFCC", "#D9F0A3", "#ADDD8E", "#78C679", "#41AB5D", "#238443", "#005A32"]           
+    when 'YlGnBu' then
+      ["#FFFFCC", "#C7E9B4", "#7FCDBB", "#41B6C4", "#1D91C0", "#225EA8", "#0C2C84"]           
+    when 'YlOrBr' then
+      ["#FFFFD4", "#FEE391", "#FEC44F", "#FE9929", "#EC7014", "#CC4C02", "#8C2D04"]           
+    end
+  end
+  
+  def edit_paths paths
+    ignore_paths = ["State_Lines", "separator", "path57"]
+    path_style   = 'font-size:12px;fill-rule:nonzero;stroke:#FFFFFF;stroke-opacity:1;stroke-width:0.1;stroke-miterlimit:4;stroke-dasharray:none;stroke-linecap:butt;marker-start:none;stroke-linejoin:bevel;fill:'
+    paths.each do |path|
+      count = counts[path['id']]
+      count ||= 0
+      next if ignore_paths.include? path['id']
+      #
+      # Brittle, change binning manually...
+      #
+      if count > 12
+        color_lvl = 6
+      elsif count > 10
+        color_lvl = 5
+      elsif count > 6
+        color_lvl = 4
+      elsif count > 4
+        color_lvl = 3
+      elsif count > 2
+        color_lvl = 2
+      elsif count > 0
+        color_lvl = 1
+      else
+        color_lvl = 0
+      end
+      #
+      color         = colors[color_lvl]
+      path['style'] = path_style + color
+    end
+  end
+
 end
 
-# Finally, output the edited svg image
-svg_map.display
+raise "\n\nUsage: ./choro_map.rb <datafile> map\n" unless ARGV.length == 2
+
+mapper = Choromap.new(ARGV[0], :color_scheme => 'YlOrRd')
+mapper.choropleth! ARGV[1]
